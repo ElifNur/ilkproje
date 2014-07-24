41724891651ba68235668972ffdb8fd6242b716c
ElifNur <en.ispirli@gmail.com>
2014-07-24 16:09:46 +0300

    market paketi eklendi

---
+++ src/market/GidaMal.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package market;
+
+import java.util.Date;
+
+
+/**
+ *
+ * @author elifnur
+ */
+public class GidaMal extends Urun {
+    String tur;
+    int  tarih;
+            
+    boolean zararlimi;
+    public int SKTfark(){
+        return 2014-tarih;
+    }
+}
